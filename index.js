05e9d607403bc4786453fd3448c390651d5d1dd6
Yasin Munye <cyasinsadiq@gmail.com>
2022-01-09 00:16:44 -0500

    added questions to file

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 // TODO: Include packages needed for this application
 const inquire = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        question: 'What is your GitHub username'
-    },
-    {
-        question: 'What is your email address'
-    },
-    {
-        question: 'What is your project name'
-    },
-    {
-        question: 'Write a description of your project'
-    },
-    {
-        question: ''
-    }
-];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquire
+ .prompt([
+    {
+        type: 'input',
+        name: 'Github:',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'Project:',
+        message: 'What is your project name?'
+    },
+    {
+        type: 'input',
+        name: 'Description:',
+        message: 'Write a description of your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license will be used?'
+        choices: ['MIT','ISC', 'APACHE']
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How to install dependecies?'
+        default: 'npm i'
+    },
+])
+ .then(data =>{
+     console.log(`Program started`)
+ });
+     
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+ 
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     node index.js
+// }
+
+// // Function call to initialize app
+// init();
